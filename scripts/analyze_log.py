5896d1525914d160f4cc83713af72616612512a4
Apochens <15309036477@qq.com>
2021-11-12 10:56:26 +0800

    Updated analyze_log.py with derivative and two apks.

--- scripts/analyze_log.py
+++ scripts/analyze_log.py
@@ -1,11 +1,13 @@
+from argparse import ArgumentParser, Namespace
+import subprocess
 import os.path
 import re
-import sys
 import json
 from datetime import datetime
 
 INIT_STATE = "state_0"
 FINAL_STATE = "Crash!"
+DERIVE_PATH = "../tools/regex"
 
 
 def print_header(header: str):
@@ -27,14 +29,91 @@ def init_counts(events: dict):
 
 def file_exists(file_path: str):
     if not os.path.exists(file_path):
-        print("file \'%s\' doesn't exists. Analysis terminated." % file_path)
+        print("[ Warning ] file \'%s\' doesn't exists. Analysis terminated." % file_path)
         return False
     return True
 
 
+def call_regex(command: str) -> str:
+    p = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p.wait()
+    if p.poll() == 0:
+        return p.communicate()[0].decode('utf-8').strip()
+    else:
+        print("[ warning ] Call RegEx failed!")
+        return ""
+
+
+class DFA:
+
+    def __init__(self, init_state: str, final_state: str, transition_function: dict):
+        self.state = init_state             # Init the state of analysis automaton
+        self.init_state = init_state
+        self.final_state = final_state
+
+        self.max_event_id = 0
+        self.tf = transition_function       # Get the automaton transition function
+        self.transition = []                # Record the current transition path
+        self.transitions: dict = dict()     # Record the happened transition chains and its occurrence time
+
+    def is_valid_transition(self, event_id: str) -> bool:
+        return event_id in self.tf[self.state]
+
+    def transit(self, event_id):     # Conduct a transition of the automaton
+        self.state = self.tf[self.state][event_id]
+        self.transition.append(event_id)
+
+    def record_transition(self):
+        if len(self.transition) > 1 and self.transition[1] != "eof":
+            key = str(self.transition)
+            if key in self.transitions:
+                self.transitions[key] += 1
+            else:
+                self.transitions[key] = 1
+
+    def reset(self, is_eof: bool):
+        if is_eof:
+            self.transition.append("eof")
+        self.record_transition()
+        self.state = INIT_STATE
+        self.transition.clear()
+
+
+class Deriver:
+
+    def __init__(self, derive_path: str, regex: str):
+        self.path = derive_path
+        self.original_regex = regex
+        self.derived_regex = regex
+        self.derivatives = dict()
+        self.min_distance = 10000
+
+    def derive(self, s: str) -> str:
+        command = self.path + " -e \"" + self.derived_regex + "\" -x \"" + s + "\" -n 1"
+        derived_regex = call_regex(command)
+        if derived_regex != self.derived_regex:
+            self.record_regex()
+        return derived_regex
+
+    def min_len(self) -> int:
+        command = self.path + " -e \"" + self.derived_regex + "\" -l"
+        return int(call_regex(command))
+
+    def reset(self):
+        if self.derived_regex != self.original_regex:
+            self.record_regex()
+            self.derived_regex = self.original_regex
+
+    def record_regex(self):
+        if self.derived_regex in self.derivatives:
+            self.derivatives[self.derived_regex] += 1
+        else:
+            self.derivatives[self.derived_regex] = 1
+
+
 class Analyzer:
 
-    def __init__(self, json_path: str):
+    def __init__(self, json_path: str, args: Namespace):
         with open(json_path, "r", encoding="utf-8") as f:
             info = json.load(f)
             self.app_name = info["app_name"]                            # Get the name of the app
@@ -51,40 +130,13 @@ class Analyzer:
             self.is_crashed = False                 # To show whether tester reaches the crash
             self.crash_time = []                    # To Record the time of crash happening
 
-            self.state = INIT_STATE                  # Init the state of analysis automaton
-            self.states = [INIT_STATE]
-            self.tf = info["transition_function"]    # Get the automaton transition function
-            self.transition = []
-            self.transitions: dict = dict()         # Record the happened transition chains and its occurrence time
+            if args.dfa:    # enable DFA
+                self.dfa = DFA(INIT_STATE, FINAL_STATE, info["transition_function"])
 
-    def is_valid_transition(self, event_id) -> bool:
-        if event_id in self.tf[self.state]:
-            return True
-        else:
-            return False
+            if args.derivative:     # enable to derive the regex
+                self.deriver = Deriver(DERIVE_PATH, info["regex"])
 
-    def record_transition(self):
-        if len(self.transition) > 1 and self.transition[1] != "eof":
-            key = str(self.transition)
-            if key in self.transitions:
-                self.transitions[key] += 1
-            else:
-                self.transitions[key] = 1
-        self.transition.clear()
-
-    def reset_automaton(self, is_eof: bool):                  # Reset the automaton
-        if is_eof:
-            self.transition.append("eof")
-        self.record_transition()
-        self.state = INIT_STATE
-        self.states.clear()
-
-    def transit_automation(self, event_id):     # Conduct a transition of the automaton
-        self.state = self.tf[self.state][event_id]
-        self.states.append(self.state)
-        self.transition.append(event_id)
-
-    def analyze(self, log_path: str, time_path: str):
+    def analyze(self, log_path: str, time_path: str, args: Namespace):
         # Process the time file
         if not file_exists(time_path):
             return False
@@ -111,19 +163,32 @@ class Analyzer:
                         if event_id not in self.first_time:     # Record the relative time of first occurrence
                             self.first_time[event_id] = event_time - start_time
 
-                        if self.is_valid_transition(event_id):     # If this is a valid transition then do it
-                            # new_state = self.tf[self.state][event_id]
-                            # if new_state in self.states and new_state != self.state:
-                            #     self.record_transition()
-                            #     self.states.clear()
-                            self.transit_automation(event_id)
-                            if self.state == FINAL_STATE:
-                                self.record_transition()
-                                self.reset_automaton(False)
-                        else:   # If the current state doesn't have a valid transition through this event
-                            self.reset_automaton(False)
-                            if self.is_valid_transition(event_id):
-                                self.transit_automation(event_id)
+                        if args.dfa:
+                            if self.dfa.is_valid_transition(event_id):     # If this is a valid transition then do it
+                                self.dfa.max_event_id = max(int(self.dfa.max_event_id), int(event_id))
+                                self.dfa.transit(event_id)
+                                if self.dfa.state == self.dfa.final_state:
+                                    self.dfa.reset(False)
+                            else:   # If the current state doesn't have a valid transition through this event
+                                self.dfa.reset(False)
+                                if self.dfa.is_valid_transition(event_id):
+                                    self.dfa.transit(event_id)
+
+                        if args.derivative:
+                            derived_regex = self.deriver.derive(event_id)
+                            if derived_regex == "P":                # If the s^(-1)R is empty set
+                                self.deriver.reset()
+                                derived_regex = self.deriver.derive(event_id)
+                                if derived_regex != "P":
+                                    self.deriver.derived_regex = derived_regex
+                            else:
+                                if derived_regex == "E":            # If the s^(-1)R is empty string
+                                    self.deriver.reset()
+                                    self.deriver.min_distance = 0
+                                else:                               # If the s^(-1)R is valid
+                                    self.deriver.derived_regex = derived_regex
+                                    # print(self.deriver.derived_regex)
+                                    self.deriver.min_distance = min(self.deriver.min_distance, self.deriver.min_len())
 
                     if re.search("Warning ", line, re.I):       # If it's a “Warning”
                         warning_id = line[re.search("Warning ", line, re.I).span()[1]]
@@ -133,11 +198,17 @@ class Analyzer:
                         self.is_crashed = True
                         self.crash_time.append(event_time - start_time)
 
-            self.reset_automaton(True)
+            if args.dfa:
+                self.dfa.reset(True)
+
+            if args.derivative:
+                self.deriver.reset()
         return True
 
     def show_result(self, tool_name: str):
         print_title("[ %s-%s (%s) ]" % (self.app_name, self.bug_id, tool_name))
+
+        # Basic module
         print_header("[ The statistics of each event ]")
         zero_count = 0      # Count up the events which are never reached
         for event in self.event_counts:
@@ -171,18 +242,36 @@ class Analyzer:
             print(" "*10 + "[ %s ] Crash occurred %d times." % (self.bug_id, len(self.crash_time)))
             print(" "*14 + "> The min time taken to reach the crash: %s" % str(self.crash_time[0]))
 
-        if len(self.transitions) != 0 and not self.is_crashed:
-            count = 1
-            print_header("[ Transitions happened in testing ]")
-            for transition in self.transitions:
-                print(" "*10 + "[Transition %d] %s" % (count, transition))
-                print(" "*14 + "Count: %s" % self.transitions[transition])
-                count += 1
+        # DFA
+        if args.dfa:
+            print_header("[ The deepest event tester have reached is %d ]" % self.dfa.max_event_id)
+
+            if args.show_all and len(self.dfa.transitions) != 0 and not self.is_crashed:
+                count = 1
+                print_header("[ Transitions happened]")
+                for transition in self.dfa.transitions:
+                    print(" "*10 + "[Transition %d] %s" % (count, transition))
+                    print(" "*14 + "Count: %s" % self.dfa.transitions[transition])
+                    count += 1
+
+        # Derivative
+        if args.derivative:
+            print_header("[ The minimum distance to the crash is %d ]" % self.deriver.min_distance)
+
+            if args.show_all and len(self.deriver.derivatives) != 0 and not self.is_crashed:
+                count = 1
+                print_header("[ Derived regexes ]")
+                for derived_regex in self.deriver.derivatives:
+                    print(" "*10 + "[Regex %d] %s" % (count, derived_regex))
+                    print(" "*14 + "Count: %d" % self.deriver.derivatives[derived_regex])
+                    count += 1
 
         print_title("[ Analysis finished ]")
 
 
-def main(log_dir: str):
+def main(args: Namespace):
+
+    log_dir = args.target_dir
 
     base_dir = os.path.basename(log_dir)
     first_pos, second_pos, third_pos, forth_pos = \
@@ -197,25 +286,28 @@ def main(log_dir: str):
         base_dir[third_pos[1]: forth_pos[0]]
 
     json_path = os.path.join("..", app_name, "configuration-" + bug_id + ".json")       # Get the path of the json file
-    analyzer = Analyzer(json_path)                                                      # Initialize an Analyzer
+    analyzer = Analyzer(json_path, args)                                                      # Initialize an Analyzer
 
     log_path = os.path.join(log_dir, "logcat.log")
     print("[ Analyzer ] The log path:\n    > %s" % log_path)
     time_path = os.path.join(log_dir, tool_name + "_testing_time_on_emulator.txt")
     print("[ Analyzer ] The time record path:\n    > %s" % time_path)
 
-    if analyzer.analyze(log_path, time_path):                                            # Start analyzing log
+    if analyzer.analyze(log_path, time_path, args):                                            # Start analyzing log
         analyzer.show_result(tool_name)
 
 
-def help_info():
-    print("[Usage]: python analyze_log.py log_path1[ log_path2[ ...]]")
-    print("[Usage]: Please put log files and the json file at the same directory with the same filename.")
-
-
 if __name__ == "__main__":
-    if len(sys.argv) <= 1:
-        help_info()
-    else:
-        for i in range(1, len(sys.argv)):
-            main(sys.argv[i])
+
+    parser = ArgumentParser(prog="Log Analyzer", usage="to analyze the output logs of running testers.")
+
+    parser.add_argument("-d", "--dfa", action="store_true", default=False, help="use dfa")
+    parser.add_argument("-D", "--derivative", action="store_true", default=False, help="use the derivative of regex")
+    parser.add_argument("-s", "--show_all", action="store_true", default=False, help="show all the valid paths")
+
+    parser.add_argument("target_dir", help="the output logs dir that to be analyzed")
+
+    args = parser.parse_args()
+    print(args)
+
+    main(args)
