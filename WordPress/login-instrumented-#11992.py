a30e67402b2bbe483f0e3dba8975a768c66c1c67
JOSEPH MA <54142435+Joseph9Morgan@users.noreply.github.com>
2021-12-16 10:48:37 +0800

    Add files via upload
    update login file for instrumented-WordPress-11992.apk

--- WordPress/login-instrumented-#11992.py
+++ WordPress/login-instrumented-#11992.py
@@ -17,24 +17,24 @@ if __name__ == '__main__':
     d = u2.connect_adb_wifi(avd_serial)
     # d.WAIT_FOR_DEVICE_TIMEOUT = 70
     wait()
-    d.app_start("org.wordpress.android")
+    d.app_start("org.wordpress.android.prealpha")
     print("start the app")
     wait()
     current_app = d.app_current()
     print(current_app)
     while True:
-        if current_app['package'] == "org.wordpress.android" and \
+        if current_app['package'] == "org.wordpress.android.prealpha" and \
                 "ui.accounts.LoginActivity" in current_app['activity']:
             break
         time.sleep(2)
 
     wait()
-    out = d(className="android.widget.Button", resourceId="org.wordpress.android:id/login_button").click()
+    out = d(className="android.widget.Button", resourceId="org.wordpress.android.prealpha:id/login_button").click()
     if not out:
         print("SUCCESS: press login button")
 
     wait()
-    out = d(className="android.widget.LinearLayout", resourceId="org.wordpress.android:id/login_site_button").click()
+    out = d(className="android.widget.LinearLayout", resourceId="org.wordpress.android.prealpha:id/login_site_button").click()
     if not out:
         print("SUCCESS: press login in via site address")
 
@@ -44,23 +44,23 @@ if __name__ == '__main__':
         print("SUCCESS: input site address")
 
     wait()
-    out = d(className="android.widget.Button", resourceId="org.wordpress.android:id/primary_button").click()
+    out = d(className="android.widget.Button", resourceId="org.wordpress.android.prealpha:id/primary_button").click()
     if not out:
         print("SUCCESS: press next")
 
     wait()
-    out = d(className="android.widget.EditText", resourceId="org.wordpress.android:id/input", text="Username").set_text(
+    out = d(className="android.widget.EditText", resourceId="org.wordpress.android.prealpha:id/input", text="Username").set_text(
         "ecnutesterjojo@163.com")
     if out:
         print("SUCCESS: input user name")
 
     wait()
-    out = d(className="android.widget.EditText", resourceId="org.wordpress.android:id/input", text="Password").set_text("Qm#ZZ^.8/9#K6&v")
+    out = d(className="android.widget.EditText", resourceId="org.wordpress.android.prealpha:id/input", text="Password").set_text("Qm#ZZ^.8/9#K6&v")
     if out:
         print("SUCCESS: input password")
 
     wait()
-    out = d(className="android.widget.Button", resourceId="org.wordpress.android:id/primary_button").click()
+    out = d(className="android.widget.Button", resourceId="org.wordpress.android.prealpha:id/primary_button").click()
     if not out:
         print("SUCCESS: press next")
 
@@ -68,7 +68,7 @@ if __name__ == '__main__':
     current_app = d.app_current()
     print(current_app['package'])
     print(current_app['activity'])
-    if current_app['package'] == "org.wordpress.android" and \
+    if current_app['package'] == "org.wordpress.android.prealpha" and \
             "ui.accounts.LoginEpilogueActivity" in current_app['activity']:
         print("****Login SUCCESS*****")
     else:
