d41a7b90fd392847434bfad7b55f3ab98e3ecba1
JOSEPH MA <54142435+Joseph9Morgan@users.noreply.github.com>
2022-02-04 13:15:44 +0800

    Update run_combodroid_login.sh

--- scripts/run_combodroid_login.sh
+++ scripts/run_combodroid_login.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 APK_FILE=$1 # e.g., xx.apk
 AVD_SERIAL=$2 # e.g., emulator-5554
 AVD_NAME=$3 # e.g., base
@@ -8,6 +7,7 @@ OUTPUT_DIR=`realpath $4`
 TEST_TIME=$5 # e.g., 10s, 10m, 10h
 HEADLESS=$6 # e.g., -no-window
 LOGIN_SCRIPT=$7 # the script for app login via uiautomator2
+IS_SNAPSHOT=$8
 
 COMBO_DIR=../tools/combodroid/combo-separate
 CURRENT_SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
@@ -103,42 +103,47 @@ config_file_name=`basename $config_file`
 
 
 sleep 2
+
 # login if necessary
 if [[ $LOGIN_SCRIPT != "" ]]
 then
-
-    # instrumented the app
-    cd $COMBO_DIR
-    adb -s ${AVD_SERIAL} uninstall ${app_package_name}
-    ./ComboDroid_instrument.sh $config_file_name >> $result_dir/combodroid.log 2>&1
-
-    cd $CURRENT_SCRIPT_DIR
     echo "** APP LOGIN (${AVD_SERIAL})"
-    python3 $LOGIN_SCRIPT ${AVD_SERIAL} combo 2>&1 | tee $result_dir/login.log
+    if [[ $IS_SNAPSHOT != "True" ]]
+    then
+        # instrumented the app
+        cd $COMBO_DIR
+        adb -s ${AVD_SERIAL} uninstall ${app_package_name}
+        ./ComboDroid_instrument.sh $config_file_name >> $result_dir/combodroid.log 2>&1
 
-    sleep 10
+        cd $CURRENT_SCRIPT_DIR
+        echo "** APP LOGIN (${AVD_SERIAL})"
+        python3 $LOGIN_SCRIPT ${AVD_SERIAL} combo 2>&1 | tee $result_dir/login.log
 
-    adb -s $avd_serial emu kill
-    exit
+        sleep 10
+
+        adb -s $avd_serial emu kill
+        exit
+    else
+      # enable if use the snapshot (already login, do not need to install the app)
+      echo " *** Login SUCCESS ****" >> $result_dir/login.log
+    fi
 
 else
     # do nothing
     echo "start to test"
     echo " *** Login SUCCESS ****" >> $result_dir/login.log
 fi
-sleep 2
 
+sleep 5
 
 # start logcat
 echo "** START LOGCAT (${AVD_SERIAL}) "
 adb -s $AVD_SERIAL logcat -c
 adb -s $AVD_SERIAL logcat -G 10M
-adb -s $AVD_SERIAL logcat AndroidRuntime:E CrashAnrDetector:D System.err:W CustomActivityOnCrash:E ACRA:E WordPress-EDITOR:E *:F *:S > $result_dir/logcat.log &
+adb -s $AVD_SERIAL logcat AndroidRuntime:E CrashAnrDetector:D System.err:W CustomActivityOnCrash:E ACRA:E WordPress-EDITOR:E Themis:I *:F *:S > $result_dir/logcat.log &
 
-# start coverage dumping
-echo "** START COVERAGE (${AVD_SERIAL}) "
-cd $CURRENT_SCRIPT_DIR
-bash dump_coverage.sh $AVD_SERIAL $app_package_name $result_dir &
+# copy dummy documents
+bash -x copy_dummy_documents.sh $avd_serial
 
 # run combodroid
 echo "** RUN COMBODROID (${AVD_SERIAL})"
@@ -147,10 +152,6 @@ adb -s $AVD_SERIAL shell date "+%Y-%m-%d-%H-%M-%S" >> $result_dir/combo_testing_
 cd $COMBO_DIR
 timeout $TEST_TIME ./ComboDroid_execute.sh $config_file_name 2>&1 | tee $result_dir/combodroid.log 
 adb -s $AVD_SERIAL shell date "+%Y-%m-%d-%H-%M-%S" >> $result_dir/combo_testing_time_on_emulator.txt
-
-# stop coverage dumping
-echo "** STOP COVERAGE (${AVD_SERIAL})"
-kill `ps aux | grep "dump_coverage.sh ${AVD_SERIAL}" | grep -v grep |  awk '{print $2}'`
 
 # stop logcat
 echo "** STOP LOGCAT (${AVD_SERIAL})"
